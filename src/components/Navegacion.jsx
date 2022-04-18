ddbbf4c71cec81e3d83adacaea9f9af8a22e6554
nfillon <fillonn@gmail.com>
2022-04-17 19:48:18 -0500

    Desafio: MenÃu e-comme

---
+++ src/components/Navegacion.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Navbar, Nav, Container, NavDropdown} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import falogo from "../assets/logofa.png";
+
+export default function Navegacion() {
+  return (
+    <div>
+      <Navbar expand={'md'} bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home"><img src={falogo} alt="logo"/></Navbar.Brand>
+          <Nav >
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#info">Informacion</Nav.Link>
+            <NavDropdown title="Restaurante" id="navbarScrollingDropdown" >
+              <NavDropdown.Item href="#roomservices">Room Services</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Resto Mexicano</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Resto Italiano</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#Menu">menu</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
